893a0f7ce5c885f5106182dcc2c0869b51aa77e1
nnreall3 <nnreall3@gmail.com>
2026-02-26 23:37:34 +0000

    fix: add basename to router and verify vite base

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/cybersite">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />

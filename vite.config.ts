c68bd23585631bf01334c4665c38044e394f25f8
nnreall3 <nnreall3@gmail.com>
2026-02-26 23:32:39 +0000

    fix: set base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/cybersite/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
